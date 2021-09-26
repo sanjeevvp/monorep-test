14c3717ca163c8b5dfe69f36f37622eb2d63eade
Sanjeev Ponnapula <sanjeevvarun8@gmail.com>
2021-09-26 21:08:18 +0100

    Updated code for mr-server

--- packages/mr-server/src/routes/index.ts
+++ packages/mr-server/src/routes/index.ts
@@ -4,7 +4,6 @@ import userRouter from "./user.route"
 const router = Router()
 
 // Return all user info
-router.use("user", userRouter)
+router.use("/user", userRouter)
 
-//testing 1233
 export default router
