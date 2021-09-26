3d157c02d791a6f6cf0f2dfe3662d7f005f6ca5d
Sanjeev Ponnapula <sanjeevvarun8@gmail.com>
2021-09-26 12:51:08 +0100

    Update user model.ts

--- packages/mr-models/src/user.model.ts
+++ packages/mr-models/src/user.model.ts
@@ -5,13 +5,18 @@ interface IUser extends Document {
   lastName: string
   email: string
   userId: string
+
+  username?: string
+  profileImage?: string
 }
 
 const IUserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  userId: { type: String, required: true, unique: true }
+  userId: { type: String, required: true, unique: true },
+  username: { type: String },
+  profileImage: { type: String }
 })
 
 const User: Model<IUser> = model<IUser>("User", IUserSchema)
